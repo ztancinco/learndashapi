d2ab958beafc939bb39f78570ca57aaa86684055
Zach Tancinco <ztancinco@fullscale.ph>
2025-01-17 21:03:20 +0800

    Update routes on courses

--- app/courses/urls.py
+++ app/courses/urls.py
@@ -8,7 +8,7 @@ from ..courses.views.quizzes_view import QuizzesView
 from ..courses.views.questions_view import QuestionsView
 
 urlpatterns = [
-    path('/', CoursesView.as_view(), name='courses-list'),
+    path('', CoursesView.as_view(), name='courses-list'),
     path('<int:course_id>/', CoursesView.as_view(), name='course-detail'),
     path('<int:course_id>/quizzes/', QuizzesView.as_view(), name='quizzes-list'),
     path('quizzes/<int:quiz_id>/questions/', QuestionsView.as_view(), name='questions-list'),
